a0593452cbd36b3a8460f67f3d175626e86e7f0d
ajuchem <ajuchem@daudt.com>
2016-03-08 13:28:43 -0700

    Add initial functions to app

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 // Solution: add interactivity so the user can manage daily tasks.
 
 // Add a new task
+var addTask = function() {
   // When button is pressed
   // Create a new list item w/ the text from #new-task:
     // input (checkbox)
@@ -10,8 +11,10 @@
     // button.edit
     // button.delete
     // each element needs to be modified and appended
+}
 
 // Edit an existing task
+var editTask = function() {
   // When edit btn pressed
     // if parent class .editMode
       // switch from .editMode
@@ -21,15 +24,22 @@
       // input value becomes labe's text
 
     // toggle .editMode on the parent
+}
 
 // Delete an existing task
+var deleteTask = function() {
   // when btn is pressed
     // remove parent list item from the ul
+}
 
 // Mark as completed
+var taskCompleted = function() {
   // when checkbox is checked
     // append task list item to #completed-task
+}
 
 // Mark as incomplete
+var taskIncompleted = function() {
   // when the checkbox is unchecked
     // append #incomplete-task
+}
